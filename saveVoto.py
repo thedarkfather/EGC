8b1880bc1e0290e223bef558650320553713f125
David Letrán <davidletrang@gmail.com>
2014-12-20 20:00:09 +0100

    Arreglada incidencia #1
    La cabecera content type no era application/json, había que dejarla por defecto

--- saveVoto.py
+++ saveVoto.py
@@ -5,7 +5,7 @@ def vote(voto,votacion_id):
     data=[('vote',voto),('votation_id',votacion_id)]
     data=urllib.urlencode(data)
     path='http://php-egc.rhcloud.com/vote.php'
-    req=urllib2.Request(path,data,headers={'Content-Type':'application/json'})
+    req=urllib2.Request(path,data)
     response=urllib2.urlopen(req)
     response_data = json.load(response)
     result = False
